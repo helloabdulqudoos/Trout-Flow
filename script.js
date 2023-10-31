a1d569e963660045209f4b3acac4daa26f88cc3b
helloabdulqudoos <145955340+helloabdulqudoos@users.noreply.github.com>
2023-10-31 18:49:46 +0000

    container config done

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 var crsr = document.querySelector("#cursor")
 var crsrBlur = document.querySelector("#cursor-blur")
 document.addEventListener("mousemove",function(dets){
-    crsr.style.left = dets.x+"px"
+    crsr.style.left = dets.x+20+"px"
     crsr.style.top = dets.y+"px"
     crsrBlur.style.left = dets.x-250+"px"
     crsrBlur.style.top = dets.y-250+"px"
@@ -15,7 +15,6 @@ gsap.to("#nav",{
     scrollTrigger : {
         trigger : "#nav",
         scroller : "body",
-        merkers : true,
         start : "top -10px",
         end : "top -11px",
         scrub : 2}
@@ -26,8 +25,22 @@ gsap.to("#main",{
     scrollTrigger : {
         trigger : "#main",
         scroller : "body",
-        merkers : true,
         start : "top -25%",
         end : "top -70%",
         scrub : 3}
+})
+
+var h4all = document.querySelectorAll("#nav h4")
+h4all.forEach(function(elem){
+    elem.addEventListener("mouseenter", function(){
+        crsr.style.scale = 2
+        crsr.style.border = "1px solid #fff"
+        crsr.style.backgroundColor = "transparent"
+    })
+    elem.addEventListener("mouseleave", function(){
+        crsr.style.scale = 1
+        crsr.style.border = "0px solid #42bcb3"
+        crsr.style.backgroundColor = "#42bcb3"
+    })
+
 })
